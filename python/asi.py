a4e3896aae8f5a62d0a015686d30e4b7fdf873e1
K. Scott Tripp <kscott.tripp@gmail.com>
2022-06-26 08:28:28 -0700

    Py: Draw  airspeed tick marks

--- python/asi.py
+++ python/asi.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle, Circle, Wedge
 import numpy as np
+
+# Geometry
+BEZEL_WIDTH = 480 # px
+FACE_DIAM = 470   # px, outer diameter of the face image
+FACE_WIDTH = 396  # px
+WINDOW_OUTER = 205 # px, outer radius of windows
+TEMP_WINDOW_INNER = 170 # px, inner radius of temperature window
+TAS_WINDOW_INNER = 162 # px, inner radius of TAS window
+
+TEMP_WINDOW_ANGLE = 40 # degrees, symmetric around vertical
+TAS_MIN_IAS = 84 # knots, min IAS covered by TAS window
+TAS_MAX_IAS = 120 # knots, max IAS covered by TAS window
+
+# Colors
+FACE_COLOR = '#131512'
+TICK_COLOR = '#FFFFFF'
+
 # Speed Range
 V_min = 35
 V_max = 170
+
+# Tick geometry
+MAJ_TICK_WIDTH = 6
+MAJ_TICK_HEIGHT = 60
+
+MIN_TICK_WIDTH = 3
+MIN_TICK_HEIGHT = 30
+
 
 # Known speed angles
 # These are the best estimates from skewed photos
@@ -33,4 +59,29 @@ plt.ylabel("Indicator angle (\u00B0)")
 
 plt.savefig("fit_function.png")
 
-plt.show()
+#################################################
+# Generate the gauge face
+
+fix, ax = plt.subplots()
+
+# Create overall geometry
+x = (FACE_DIAM/2-1) * np.sin(2*np.pi* np.array([0, 90, 180, 270])/360)
+y = (FACE_DIAM/2-1) * np.cos(2*np.pi* np.array([0, 90, 180, 270])/360)
+ax.plot(x, y, '.', color=FACE_COLOR)
+ax.axis("Off")
+plt.gca().set_aspect('equal')
+
+
+# Draw ticks
+for idx, angle in enumerate(angles):
+    theta = 2*np.pi * angle/360
+    x = (FACE_WIDTH/2) * np.cos(theta)
+    y = (FACE_WIDTH/2) * np.sin(theta)
+
+    width = MAJ_TICK_WIDTH/2 if idx % 2 == 1 else MIN_TICK_WIDTH/2
+    height = MAJ_TICK_HEIGHT if idx % 2 == 1 else MIN_TICK_HEIGHT
+
+    ax.add_patch(Rectangle((x,y),  1 * width, -1 * height, angle-90, color=TICK_COLOR))
+    ax.add_patch(Rectangle((x,y), -1 * width, -1 * height, angle-90, color=TICK_COLOR))
+plt.savefig("guage_face.png", transparent=True)
+#plt.show()
